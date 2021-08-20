8fe6c6cea555815732597e4187534f775e2740e4
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-20 16:23:11 +0900

    update: resolve requests in js,html

--- byte-degree-html/src/assets/js/main.js
+++ byte-degree-html/src/assets/js/main.js
@@ -1,7 +1,11 @@
 // 글쓰기 버튼 페이지 이동 방지
 const $submit = document.querySelector('button[type="submit"]');
-$submit &&
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    alert("DOM!");
+    $submit &&
     $submit.addEventListener("click", e => {
         e.preventDefault();
         alert("Great!");
     });
+});
